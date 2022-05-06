a52381b9f7cb33d0d5381b24a2b370cffdfd09a5
Frank McCown <fmccown@harding.edu>
2022-05-06 16:24:15 -0500

    Update all packages

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 // db.js
 const mongoose = require("mongoose");
-mongoose.set("useUnifiedTopology", true);
 mongoose.connect("mongodb://localhost/musicdb", 
    { useNewUrlParser: true });
 module.exports = mongoose;
